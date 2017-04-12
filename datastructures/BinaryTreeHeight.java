bc97f7ed8ad7982e1f367ea832166c32b038beff
Leszek Zychowski <maxstone9@gmail.com>
2017-04-11 19:03:45 -0500

    added binary tree height

---
+++ datastructures/BinaryTreeHeight.java
@@ -0,0 +1,29 @@
+class BinaryTreeHeight {
+    
+    static int leftHeight = 0;
+    static int rightHeight = 0;
+
+	static int height(Node root) {
+        return findHeight(root) - 1;
+    }
+
+    static int findHeight(Node node){
+        if (node != null){
+            return Math.max(findHeight(node.left), findHeight(node.right)) + 1;
+        } else {
+            return 0;
+        }
+    }
+
+    /*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+
+    public static void main(String[] args) {
+        // Instantiate new LinkedList based on Node above
+        // System.out.println(height(root));
+    }	
+}
